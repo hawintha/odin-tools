ac7674086297c7a4fd5f8b339cc3b4e5295fbd77
Ha <99289909+hawintha@users.noreply.github.com>
2022-06-22 10:28:43 -0700

    Automatically add new movies to table and allow deletion.

--- tools/library.js
+++ tools/library.js
@@ -8,20 +8,51 @@ class Movie {
     }
 }
 
-const findingNemo = new Movie("Finding Nemo", 2003, "watched");
-movies.push(findingNemo)
-
+movies.push(new Movie("Finding Nemo", 2003, "Watched"));
+let id = 1;
 function addMovie(title, year, watchStatus) {
     let newMovie = new Movie(title, year, watchStatus)
     movies.push(newMovie);
     console.log(movies);
+    const table = document.querySelector('table');
+    const row = table.insertRow();
+    row.dataset.id = id;
+    id++;
+    const titleCell = row.insertCell();
+    titleCell.textContent = title
+    const yearCell = row.insertCell();
+    yearCell.textContent = year
+    const statusCell = row.insertCell();
+    statusCell.textContent = watchStatus;
+    const actionCell = row.insertCell();
+    const updateBtn = document.createElement('button')
+    updateBtn.classList.add("updateBtn");
+    updateBtn.textContent = "Update";
+    actionCell.appendChild(updateBtn);
+    const editBtn = document.createElement('button')
+    editBtn.classList.add("editBtn");
+    editBtn.textContent = "Edit";
+    actionCell.appendChild(editBtn);
+    const deleteBtn = document.createElement('button')
+    deleteBtn.classList.add("deleteBtn");
+    deleteBtn.textContent = "Delete";
+    actionCell.appendChild(deleteBtn);
 }
 
-const addBtn = document.querySelector('#addBtn');
-addBtn.addEventListener('click', () => {
+document.querySelector('body').addEventListener('click', (e) => {
     const titleInput = document.querySelector('#titleInput');
     const yearInput = document.querySelector('#yearInput');
     const watchStatus = document.querySelector('#watchStatus');
-    addMovie(titleInput.value, yearInput.value, watchStatus.value);
-    titleInput.value = "", yearInput.value = "", watchStatus.value = "watched";
+    if (e.target.id === "addBtn" && titleInput.value) {
+        addMovie(titleInput.value, yearInput.value, watchStatus.value);
+        titleInput.value = "", yearInput.value = "", watchStatus.value = "Watched";
+    }
+    else if (e.target.classList.contains("deleteBtn")) {
+        const title = e.target.parentElement.parentElement.firstElementChild.textContent;
+        movies = movies.filter(function (movie) {
+            return movie.title !== title;
+        })
+        console.log(movies);
+        e.target.parentElement.parentElement.remove();
+    }
 })
