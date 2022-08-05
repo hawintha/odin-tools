2e6ff3188c64e665c9329b0f69b345f71f4380cb
Ha <99289909+hawintha@users.noreply.github.com>
2022-08-04 20:14:52 -0700

    Integrate update button with watch status and prevent index from resetting early

--- tools/library.js
+++ tools/library.js
@@ -1,5 +1,4 @@
 let movies = [];
-
 class Movie {
     constructor(title, year, watchStatus) {
         this.title = title;
@@ -10,6 +9,7 @@ class Movie {
 movies.push(new Movie("The Lion King", 1994, "Watched"));
 movies.push(new Movie("Finding Nemo", 2003, "Watched"));
 movies.push(new Movie("Zootopia", 2016, "Watched"));
+
 function createBtn(className, text, cell) {
     const btn = document.createElement('button');
     btn.classList.add(className);
@@ -21,20 +21,16 @@ function addMovie(title, year, watchStatus) {
     movies.push(newMovie);
     const tbody = document.querySelector('tbody');
     const row = tbody.insertRow();
-
     const titleCell = row.insertCell();
     titleCell.textContent = title;
     const yearCell = row.insertCell();
     yearCell.textContent = year;
     const statusCell = row.insertCell();
-    statusCell.textContent = watchStatus;
+    createBtn("statusBtn", watchStatus, statusCell);
     const actionCell = row.insertCell();
-
-    createBtn("updateBtn", "Update", actionCell);
     createBtn("editBtn", "Edit", actionCell);
     createBtn("deleteBtn", "Delete", actionCell);
 }
-
 function findMovieIndex(title) {
     for (let i = 0; i < movies.length; i++) {
         if (movies[i].title === title) {
@@ -42,31 +38,28 @@ function findMovieIndex(title) {
         }
     }
 }
-
 function deleteMovie(index, node) {
     movies.splice(index, 1)
     node.remove();
 }
 
+let index = 0;
 document.querySelector('body').addEventListener('click', (e) => {
     let titleInput = document.querySelector('#titleInput');
     let yearInput = document.querySelector('#yearInput');
     let statusSelect = document.querySelector('#statusSelect');
-    let index = 0;
     const watchStatus = e.target.parentElement.previousElementSibling.textContent;
-
     if (e.target.id === "addBtn" && titleInput.value) {
         addMovie(titleInput.value, yearInput.value, statusSelect.value);
         titleInput.value = ""; yearInput.value = ""; statusSelect.value = "Watched";
-    }
-    else if (e.target.id === "saveBtn") {
+    } else if (e.target.id === "saveBtn") {
         movies[index].title = titleInput.value;
         movies[index].year = yearInput.value;
         movies[index].watchStatus = statusSelect.value;
         const editingRow = document.querySelector('.editing');
         editingRow.firstElementChild.textContent = titleInput.value;
         editingRow.children.item(1).textContent = yearInput.value;
-        editingRow.children.item(2).textContent = statusSelect.value;
+        editingRow.children.item(2).firstElementChild.textContent = statusSelect.value;
         editingRow.classList.remove("editing");
         index = 0;
         const saveBtn = document.querySelector('#saveBtn');
@@ -75,20 +68,20 @@ document.querySelector('body').addEventListener('click', (e) => {
         titleInput.value = ""; yearInput.value = ""; statusSelect.value = "Watched";
     }
 
-    if (e.target.classList.contains("updateBtn")) {
-        if (watchStatus === "Watched") {
-            e.target.parentElement.previousElementSibling.textContent = "Unwatched";
-            movies[findMovieIndex(e.target.parentElement.parentElement.firstElementChild.textContent)].watchStatus = "Unwatched";
+    const movieTitle = e.target.parentElement.parentElement.firstElementChild.textContent;
+    if (e.target.classList.contains("statusBtn")) {
+        if (e.target.textContent === "Watched") {
+            e.target.textContent = "Unwatched";
+            movies[findMovieIndex(movieTitle)].watchStatus = "Unwatched";
         } else {
-            e.target.parentElement.previousElementSibling.textContent = "Watched";
-            movies[findMovieIndex(e.target.parentElement.parentElement.firstElementChild.textContent)].watchStatus = "Watched";
+            e.target.textContent = "Watched";
+            movies[findMovieIndex(movieTitle)].watchStatus = "Watched";
         }
-    }
-    else if (e.target.classList.contains("editBtn")) {
-        titleInput.value = e.target.parentElement.parentElement.firstElementChild.textContent;
+    } else if (e.target.classList.contains("editBtn")) {
+        titleInput.value = movieTitle;
         yearInput.value = e.target.parentElement.parentElement.children.item(1).textContent;
         statusSelect.value = watchStatus;
-        index = findMovieIndex(e.target.parentElement.parentElement.firstElementChild.textContent);
+        index = findMovieIndex(movieTitle);
         if (document.querySelector('.editing')) {
             document.querySelector('.editing').classList.remove("editing");
         }
@@ -98,9 +91,8 @@ document.querySelector('body').addEventListener('click', (e) => {
             addBtn.textContent = "Save";
             addBtn.id = "saveBtn";
         }
-    }
-    else if (e.target.classList.contains("deleteBtn")) {
-        deleteMovie(findMovieIndex(e.target.parentElement.parentElement.firstElementChild.textContent), e.target.parentElement.parentElement)
+    } else if (e.target.classList.contains("deleteBtn")) {
+        deleteMovie(findMovieIndex(movieTitle), e.target.parentElement.parentElement)
     }
     console.log(movies);
 });
